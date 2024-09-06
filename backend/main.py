b0a1b3f0ed414b9d4305888ba35a8949c2d55168
blaizot <guillaume.blaizot@epitech.eu>
2024-09-06 16:36:16 +0200

    fix(api): fix events issues with max_participants

--- backend/main.py
+++ backend/main.py
@@ -136,13 +136,20 @@ class api_events(BaseModel):
     id: int
     name: str
     date: str
-    max_partcipants: int
+    duration: int
+    max_participants: int
+    location_x: str
+    location_y: str
+    type: str
+    employee_id: int
+    location_name: str
 
 class api_event_id(BaseModel):
     id: int
     name: str
     date: str
-    max_partcipants: int
+    duration: int
+    max_participants: int
     location_x: str
     location_y: str
     type: str
@@ -749,18 +756,6 @@ def get_events():
         return events
     except Exception as e:
         print(traceback.format_exc())
-        raise HTTPException(status_code=500, detail=str(e))
-
-
-@app.get("/api/tips",
-            response_model=List[api_tips],
-            tags=["tips"])
-def get_tips():
-    try:
-        collection = database.tips
-        tips = list(collection.find({}, {"_id": 0}))
-        return tips
-    except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
 
