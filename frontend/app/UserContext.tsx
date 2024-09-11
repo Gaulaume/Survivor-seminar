b38cf25e6c5645a7155dc7db5191ae1231da1b3b
LouisLanganay <louis.langanay@epitech.eu>
2024-09-11 20:42:38 +0200

    feat(UserContext): Add loading state and progress bar
    - Added loading state and progress bar to UserContext component
    - The is used to a loading message while fetching user data
    - The progress bar is updated during the fetch process to indicate the progress
     an error the fetch, an error message is displayed- If the fetch is successful, the user data set in the state
    - The loading state and are displayed until the complete

--- frontend/app/UserContext.tsx
+++ frontend/app/UserContext.tsx
@@ -2,6 +2,7 @@ import React, { createContext, useState, useContext, useEffect } from 'react';
 import { getMe } from '@/api/Employees';
 import { useAuth } from './actions';
 import Employee from '@/types/Employee';
+import { Progress } from '@/components/ui/progress';
 
 interface UserContextType {
   user: Employee | null;
@@ -12,26 +13,42 @@ const UserContext = createContext<UserContextType | undefined>(undefined);
 
 export const UserProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [user, setUser] = useState<Employee | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [progress, setProgress] = useState(0);
   const { getToken } = useAuth();
-
-  console.log("USER, ", user);
 
   useEffect(() => {
     const fetchUser = async () => {
       try {
+        setProgress(25);
         const token = getToken();
+        setProgress(50);
         const userData = await getMe(token);
+        setProgress(75);
         if (!userData) throw new Error('User not found');
         setUser(userData);
+        setProgress(100);
       } catch (error) {
         console.error('Failed to fetch user', error);
+      } finally {
+        setLoading(false);
       }
     };
 
-    if (!user) {
+    if (!user)
       fetchUser();
-    }
   }, []);
+
+  if (loading)
+    return (
+      <div className='flex flex-col items-center justify-center h-screen'>
+        <h2 className='text-xl font-bold mb-4'>Loading...</h2>
+        <Progress value={progress} className='w-1/4 mb-4' />
+      </div>
+    );
+
+  if (!user)
+    return <div>Error: Unable to load user data</div>;
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
@@ -42,8 +59,7 @@ export const UserProvider: React.FC<{ children: React.ReactNode }> = ({ children
 
 export const useUser = () => {
   const context = useContext(UserContext);
-  if (context === undefined) {
+  if (context === undefined)
     throw new Error('useUser must be used within a UserProvider');
-  }
   return context;
 };
